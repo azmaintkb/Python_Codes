cbd6397f2c0b78d42b841bd75bf40fe54e6da87e
Azmain Takib <Azmaintakibb@gmail.com>
2026-06-28 00:40:53 +0600

    Version 1

---
+++ start.py
@@ -0,0 +1,57 @@
+#print("LOL")
+#print("1","2","3")
+
+#for( i=0;i<10:i++):
+      #print(i)
+# for i in range(10,1,-2):
+#     print(i)
+
+# #name = ("Shabaaaa")
+# age =60
+# lola='A'
+# #print(lola)
+
+# #print(name)
+# print(type(age))
+
+# #weight = input()
+# #print(weight)
+# i=0
+# while i<5:
+#     print(i
+
+
+# def mul(a,x):
+#     return a*x
+
+# sol=mul(4,7)
+# print(sol)
+#int arr[10] a[9]=2
+
+# arr=[]
+# arr.append(4)
+# arr.append(5)
+# print(arr)
+#arrr=[0]*10
+#arrr[3]=7
+#print(arrr)
+#print(len(arrr))
+
+
+class NeedProtein:
+    def __init__(self,source_0="str",p0=0,source_1="str2",p1=1):
+      self.source_0 = source_0
+      self.p0 = p0
+      self.source_1 = source_1
+      self.p1 =p1
+
+np1 = NeedProtein()
+
+# print(np1.p1)
+# print(np1.source_1)
+
+np2 = NeedProtein("soya",50)
+print(np2.source_0)
+print(np2.p0)
+
+
